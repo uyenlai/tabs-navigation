54b9e108d5c311f79e0abad25a06473da6774b46
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-18 01:14:21 +0200

    project finished

---
+++ tabJS.js
@@ -0,0 +1,17 @@
+const panes = document.querySelectorAll(".tab-pane");
+const tabs = document.querySelectorAll(".tab-item");
+const line = document.querySelector(".tabs .line");
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+  tab.onclick = function () {
+    document.querySelector(".tab-item.active").classList.remove("active");
+    tab.classList.add("active");
+
+    line.style.left = tab.offsetLeft + "px";
+    line.style.width = tab.offsetWidth + "px";
+
+    document.querySelector(".tab-pane.active").classList.remove("active");
+    pane.classList.add("active");
+  };
+});
